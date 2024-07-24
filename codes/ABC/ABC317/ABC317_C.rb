6e753ae5a2bc12d109475bb71f3204c77344e755
A-M-1123 <akiho.m1123@gmail.com>
2024-07-24 14:47:34 +0900

    fix ABC317-C

--- codes/ABC/ABC317/ABC317_C.rb
+++ codes/ABC/ABC317/ABC317_C.rb
@@ -1,60 +1,45 @@
-T=true; F=false
-def putsTF(bool,strT,strF);(bool)?(puts strT):(puts strF);end; def putsYN(bool);putsTF(bool,"Yes","No");end
-# putsTF(bool, strT, strF) putsYN(bool)
-def puts2DArray(arr,part);arr.each{|ae| puts ae.join(part)};end
-# puts2DArray(arr,partition)
-def copyArray(a);return Marshal.load(Marshal.dump(a));end
-# copyArray(array)
-def addMargin2DArray(a,c);h = a.length;w = a[0].length;for i in 0..(h-1);a[i].unshift(c);a[i].push(c);end;a.unshift(Array.new(w+2,c));a.push(Array.new(w+2,c));return a;end
-# addMargin2DArray(arr,char)
-def trimPos2DArray(a,c);h=a.length;w=a[0].length;hRng=[0,h-1];wRng=[0,w-1];hExs=Array.new(h,F);wExs=Array.new(w,F);for i in 0..(h-1);for j in 0..(w-1);if a[i][j]==c;hExs[i]=T;wExs[j]=T;end;end;end;for i in 0..(h-1);if hExs[i];hRng[0]=i;break;end;end;for i in 0..(h-1);if hExs[h-1-i];hRng[1]=h-1-i;break;end;end;for i in 0..(w-1);if wExs[i];wRng[0]=i;break;end;end;for i in 0..(w-1);if wExs[w-1-i];wRng[1]=w-1-i;break;end;end;return[hRng,wRng];end; def trim2DArray(a,pos);hRng,wRng=pos;raH=hRng[1]-hRng[0]+1;raW=wRng[1]-wRng[0]+1;retA=Array.new(raH).map{Array.new(raW)};for i in 0..(raH-1);for j in 0..(raW-1);retA[i][j]=a[hRng[0]+i][wRng[0]+j];end;end;return retA;end
-# trimPos2DArray(arr,remainMark) trim2DArray(arr,pos):pos...2*2-Array
-def makeNds(n,m,a,if1way,ifWgt,ifLbl,ifTree);nds=Array.new(n+1).map{Array.new(0)};for i in 0..(m-1);id1=a[i][0];id2=a[i][1];w=a[i][2]if ifWgt;if !ifWgt&&!ifLbl;nds[id1].push(id2);nds[id2].push(id1)if !if1way;else;ed=[id2];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id1].push(ed);ed=[id1];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id2].push(ed)if !if1way;end;end;n.times{|i|nds[i+1].push(-1)}if if1way||!ifTree;return nds;end
-# makeNds(ndN,arrL,array,1way?,wgt?,lbl?,tree?) template--makeNds(n,m,a,F,F,F,F)
-def bins(a,t,st,en);if st>en;puts "Err...bins: st>en";exit;end;m=(st+en)/2;return m if t==a[m];return-1if st==en||t<a[st]||a[en]<t;(t<a[m])?(s,e=st,m-1):(s,e=m+1,en);return bins(a,t,s,e);end
-# bins(array,target,0,n-1)
-def getPFSum(a);l=a.length;r=Array.new(l+1,0);for i in 0..(l-1);r[i+1]=r[i]+a[i];end;return r;end; def calcPFSum(a,s,e);return a[e+1]-a[s];end
-# getPFSum(array) calcPFSum(array,st,en)
+T=true; F=false; INF=9999999999999 # 10**13-1
+def makeNds(n,m,a,if1way,ifWgt,ifLbl,ifTer);nds=Array.new(n+1).map{Array.new(0)};i=0;while i<m;id1,id2=a[i][0],a[i][1];w=a[i][2]if ifWgt;if !ifWgt && !ifLbl;nds[id1].push(id2);nds[id2].push(id1)if !if1way;else;ed=[id2];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id1].push(ed);if !if1way;ed=[id1];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id2].push(ed);end;end;i+=1;end;n.times{|i|nds[i+1].push(nil)}if if1way || !ifTer;return nds;end
+def getDigitLen(n);n=n.abs;return 1 if n==0;return Math.log10(n).to_i+1;end; def getNumApart(n);d=[];begin;d.unshift(n%10);n/=10;end while n>0;return d;end
+def to1d(y,x,w); return y*w+x; end;  def to2d(n,w); return (n / w),(n % w); end
+def nC2(n); return 0 if n<2; return n*(n-1)/2; end;  def sumStep(n); return 0 if n<1; return n*(n+1)/2; end;  def count0011(w,i); blc = n/(w << 1); ret = blc*w; n -= blc*(w << 1)+w-1; ret+=n if 0<n; return ret; end
+def sort2darr(a);a.sort!{|x,y|(x[0]<=>y[0]).nonzero?||x[1]<=>y[1]};end
+def copyArr(a);return Marshal.load(Marshal.dump(a));end
+def pTF(bool,strT,strF);(bool)?(puts strT):(puts strF);end; def pYN(bool);pTF(bool,"Yes","No");end
+def p2darr(arr,part);arr.each{|ae| puts ae.join(part)};end
 def gInt();gets.to_i;end; def gInts();gets.split.map(&:to_i);end; def gIntMat(n);n.times.map{gInts()};end; def gIntVars(n);gIntMat(n).transpose;end
-# g...Int Ints IntMat(n) IntVars(n)
 def gStrs();gets.chomp.split;end; def gCharsDiv();gets.chomp.split("");end; def gCharMat(n);n.times.map{gets.chomp.split("")};end; def gStrsVert(n);n.times.map{gets.chomp};end
+# makeNds(ndN,edgeN,edgeArr,1way?,wgt?,lbl?,spanning?) <ex.makeNds(n,m,a,F,F,F,T)>
+# getDigitLen(num) # getNumApart(num) <ex.4275=>[4,2,7,5]>
+# to1d(y,x,w) to2d(n,w) <w = width>
+# nCn(n) # sumStep(n) <sum(1..n)> # count0011(w,i) <0-index>
+# sort2darr(array) <for 2*2-array>
+# copyArr(array)
+# p...TF(bool,strT,strF) YN(bool) 2darr(arr,partition)
+# g...Int Ints IntMat(n) IntVars(n)
 # g...Strs CharsDiv CharMat(n) StrsVert(n)
 
-INF = 999999999999
-
-def solve(n,nds,st)
-  ans = 0
-  vis = "0"*(n+1)
-  vis[st] = "1"
-  que = [[st,vis,0]]
-  len = 1
-  i = 0
-  while i < len
-    curID,curVis,curD = que[i]
-    nds[curID].each do |e|
-      next if e == -1
-      nxtID,nxtD = e
-      if curVis[nxtID] == "0"
-        nxtVis = curVis.dup
-        nxtVis[nxtID] = "1"
-        que.push([nxtID,nxtVis,curD+nxtD])
-        len += 1
-        ans = [ans,curD+nxtD].max
-      end
-    end
-    i += 1
-  end
-  return ans
+n,m = gInts
+dist = Array.new(n).map{Array.new(n,F)}
+i = -1
+while (i += 1) < m
+  a,b,c = gInts
+  dist[a-1][b-1] = c
+  dist[b-1][a-1] = c
 end
 
-n,m = gInts
-abc = gIntMat(m)
-
-nds = makeNds(n,m,abc,F,T,F,F)
-
-ans = 0
-for st in 1..n
-  ret = solve(n,nds,st)
-  ans = [ans,ret].max
+ans = d = 0
+((0..(n-1)).to_a).permutation do |ord|
+  sum = 0
+  i = -1
+  while (i += 1) < n-1
+    if (d = dist[ord[i]][ord[i+1]])
+      sum += d
+    else
+      ans = sum if ans < sum
+      sum = 0
+    end
+  end
+  ans = sum if ans < sum
 end
 puts ans
