a3dbc81f82a874f7088c2fed5755f62989d5c67c
A-M-1123 <akiho.m1123@gmail.com>
2024-07-24 14:34:40 +0900

    fix ABC264-C

--- codes/ABC/ABC264/ABC264_C.rb
+++ codes/ABC/ABC264/ABC264_C.rb
@@ -1,34 +1,52 @@
-T=true; F=false
-def putsTF(bool,strT,strF);(bool)?(puts strT):(puts strF);end
-# putsTF(bool, strT, strF)
-def copyArray(a);return Marshal.load(Marshal.dump(a));end
-# copyArray(array)
-def makeNds(n,m,a,if1way,ifWgt,ifLbl,ifTree);nds=Array.new(n+1).map{Array.new(0)};for i in 0..(m-1);id1=a[i][0];id2=a[i][1];w=a[i][2]if ifWgt;if !ifWgt&&!ifLbl;nds[id1].push(id2);nds[id2].push(id1)if !if1way;else;ed=[id2];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id1].push(ed);ed=[id1];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id2].push(ed)if !if1way;end;end;n.times{|i|nds[i+1].push(-1)}if if1way||!ifTree;return nds;end
-# makeNds(ndN,arrL,array,1way?,wgt?,lbl?,tree?) template--makeNds(n,m,a,F,F,F,F)
-def bins(a,t,st,en);if st>en;puts "Err...bins: st>en";exit;end;m=(st+en)/2;return m if t==a[m];return-1if st==en||t<a[st]||a[en]<t;(t<a[m])?(s,e=st,m-1):(s,e=m+1,en);return bins(a,t,s,e);end
-# bins(array,target,0,n-1)
-def getPFSum(a);l=a.length;r=Array.new(l+1,0);for i in 0..(l-1);r[i+1]=r[i]+a[i];end;return r;end; def calcPFSum(a,s,e);return a[e+1]-a[s];end
-# getPFSum(array) calcPFSum(array,st,en)
+T=true; F=false; INF=9999999999999 # 10**13-1
+def makeNds(n,m,a,if1way,ifWgt,ifLbl,ifTer);nds=Array.new(n+1).map{Array.new(0)};i=0;while i<m;id1,id2=a[i][0],a[i][1];w=a[i][2]if ifWgt;if !ifWgt && !ifLbl;nds[id1].push(id2);nds[id2].push(id1)if !if1way;else;ed=[id2];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id1].push(ed);if !if1way;ed=[id1];ed.push(w)if ifWgt;ed.push(i+1)if ifLbl;nds[id2].push(ed);end;end;i+=1;end;n.times{|i|nds[i+1].push(nil)}if if1way || !ifTer;return nds;end
+def getDigitLen(n);n=n.abs;return 1 if n==0;return Math.log10(n).to_i+1;end; def getNumApart(n);d=[];begin;d.unshift(n%10);n/=10;end while n>0;return d;end
+def to1d(y,x,w); return y*w+x; end;  def to2d(n,w); return (n / w),(n % w); end
+def nC2(n); return 0 if n<2; return n*(n-1)/2; end;  def sumStep(n); return 0 if n<1; return n*(n+1)/2; end;  def count0011(w,i); blc = n/(w << 1); ret = blc*w; n -= blc*(w << 1)+w-1; ret+=n if 0<n; return ret; end
+def sort2darr(a);a.sort!{|x,y|(x[0]<=>y[0]).nonzero?||x[1]<=>y[1]};end
+def copyArr(a);return Marshal.load(Marshal.dump(a));end
+def pTF(bool,strT,strF);(bool)?(puts strT):(puts strF);end; def pYN(bool);pTF(bool,"Yes","No");end
+def p2darr(arr,part);arr.each{|ae| puts ae.join(part)};end
 def gInt();gets.to_i;end; def gInts();gets.split.map(&:to_i);end; def gIntMat(n);n.times.map{gInts()};end; def gIntVars(n);gIntMat(n).transpose;end
+def gStrs();gets.chomp.split;end; def gCharsDiv();gets.chomp.split("");end; def gCharMat(n);n.times.map{gets.chomp.split("")};end; def gStrsVert(n);n.times.map{gets.chomp};end
+# makeNds(ndN,edgeN,edgeArr,1way?,wgt?,lbl?,spanning?) <ex.makeNds(n,m,a,F,F,F,T)>
+# getDigitLen(num) # getNumApart(num) <ex.4275=>[4,2,7,5]>
+# to1d(y,x,w) to2d(n,w) <w = width>
+# nCn(n) # sumStep(n) <sum(1..n)> # count0011(w,i) <0-index>
+# sort2darr(array) <for 2*2-array>
+# copyArr(array)
+# p...TF(bool,strT,strF) YN(bool) 2darr(arr,partition)
 # g...Int Ints IntMat(n) IntVars(n)
-def gChars();gets.chomp.split;end; def gCharsDiv();gets.chomp.split("");end; def gCharMat(n);n.times.map{gets.chomp.split("")};end; def gStrs(n);n.times.map{gets.chomp};end; def gQuery(r,c,t);return[] if c!=t.length;a=r.times.map{gets.chomp.split};return a if t.uniq==[0];for i in 0..(c-1);next if t[i]==0;for j in 0..(r-1);(t[i]==1)?(a[j][i]=a[j][i].to_i):(a[j][i]=a[j][i].to_f);end;end;return a;end
-# g...Chars CharsDiv CharMat(n) Strs(n) Query(row,col,[s0,i1,f2])
+# g...Strs CharsDiv CharMat(n) StrsVert(n)
 
-r,c = gInts
+h1,w1 = gInts
+a = gIntMat(h1)
+h2,w2 = gInts
+b = gIntMat(h2)
 
-bw = Array.new(15).map{Array.new(15,-1)}
-for i in 0..7
-  col = i % 2
-  for j in i..(i+(7-i)*2)
-    bw[i][j] = col
-    bw[j][i] = col
-    bw[14-i][14-j] = col
-    bw[14-j][14-i] = col
+hArr = (0..(h1-1)).to_a
+wArr = (0..(w1-1)).to_a
+ans = F
+hArr.combination(h2) do |ys|
+  wArr.combination(w2) do |xs|
+    flag = T
+    y = -1
+    while (y += 1) < h2
+      x = -1
+      while (x += 1) < w2
+        if a[ys[y]][xs[x]] != b[y][x]
+          flag = F
+          break
+        end
+      end
+      break if !flag
+    end
+    
+    if flag
+      ans = T
+      break
+    end
   end
+  break if ans
 end
-
-if bw[r-1][c-1] == 0
-  puts "black"
-elsif bw[r-1][c-1] == 1
-  puts "white"
-end
+pYN(ans)
