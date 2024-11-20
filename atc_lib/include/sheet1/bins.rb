273dd3152bbfba362e5397cc7bd3bd3dbf32e81e
A-M-1123 <akiho.m1123@gmail.com>
2024-11-21 08:53:36 +0900

    fix SegTree

--- atc_lib/include/sheet1/bins.rb
+++ atc_lib/include/sheet1/bins.rb
@@ -1,6 +1,5 @@
-def binsJust(a,t,n)
-    return nil if t < a[0] || a[-1] < t
-    l,r = 0,n-1
+def binsJust(a,t,l,r)
+    return nil if t < a[l] || a[r] < t
     while l <= r
         m = (l+r)/2
         return m if a[m] == t
@@ -53,5 +52,5 @@ def binsUnder(a,t,n)
     return rm
 end
 
-def binsJust(a,t,n);return nil if t<a[0]||a[-1]<t;l,r=0,n-1;while l<=r;m=(l+r)/2;return m if a[m]==t;(a[m]<t)?(l=m+1):(r=m-1);end;return nil;end; def binsJustOver(a,t,n);return nil if a[-1]<t;l,r=0,n;while l<r;m=(l+r)/2;(a[m]<t)?(l=m+1):(r=m);end;return l;end; def binsOver(a,t,n);return nil if a[-1]<=t;l,r=0,n;while l<r;m=(l+r)/2;(a[m]<=t)?(l=m+1):(r=m);end;return l;end; def binsJustUnder(a,t,n);return nil if t<a[0];l,r=0,n-1;rm=0;while l<=r;m=(l+r)/2;(a[m]<=t)?(l=m+1; rm=m if rm<m):(r=m-1);end;return rm;end; def binsUnder(a,t,n);return nil if t<=a[0];l,r=0,n-1;rm=0;while l<=r;m=(l+r)/2;(a[m]<t)?(l=m+1; rm=m if rm<m):(r=m-1);end;return rm;end
+def binsJust(a,t,l,r);return nil if t<a[l]||a[r]<t;while l<=r;m=(l+r)/2;return m if a[m]==t;(a[m]<t)?(l=m+1):(r=m-1);end;return nil;end; def binsJustOver(a,t,n);return nil if a[-1]<t;l,r=0,n;while l<r;m=(l+r)/2;(a[m]<t)?(l=m+1):(r=m);end;return l;end; def binsOver(a,t,n);return nil if a[-1]<=t;l,r=0,n;while l<r;m=(l+r)/2;(a[m]<=t)?(l=m+1):(r=m);end;return l;end; def binsJustUnder(a,t,n);return nil if t<a[0];l,r=0,n-1;rm=0;while l<=r;m=(l+r)/2;(a[m]<=t)?(l=m+1; rm=m if rm<m):(r=m-1);end;return rm;end; def binsUnder(a,t,n);return nil if t<=a[0];l,r=0,n-1;rm=0;while l<=r;m=(l+r)/2;(a[m]<t)?(l=m+1; rm=m if rm<m):(r=m-1);end;return rm;end
 # binsJust & binsJustOver & binsOver & binsJustUnder & binsUnder (array,target,len)
